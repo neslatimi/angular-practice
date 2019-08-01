8f58d5fb8f8649565e56d7d5b1324ffaf6760007
neslanovic <timy1901@gmail.com>
2019-08-01 09:30:57 +0200

    changes

--- data-binding/src/app/app.component.ts
+++ data-binding/src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  title:string='angular-practice-001';
+  getName():string{
+     return `Bond, James Bond`;
+    }
+    names:string[]=[
+    'Pisti',
+    'Sanyi',
+    'Laci',
+    'Vali'
+  ];
+  name:string;
+  disabled:boolean=false;
 
+  constructor(){
+    this.name=this.names[0];
+    setInterval(()=>{
+    let index:number=Math.floor(Math.random()*this.names.length);
+    //this.name=this.names[index];
+   // this.disabled=!this.disabled;
+  },2000);
+  }
+  setName(ev:MouseEvent):void{
+    console.log(ev);
+  }
 }
